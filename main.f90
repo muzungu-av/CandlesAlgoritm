87af3fdb63db099be4682c1b8161377da29fb73d
Valera <val12042025@gmail.com>
2025-09-25 12:47:36 +0300

    типа для свечи

--- main.f90
+++ main.f90
@@ -5,50 +5,52 @@ program main
     use levels_mod
     implicit none
 
+    ! Определение типа для свечи
+    type :: candle_t
+        real(c_double) :: x, open, high, low, close
+    end type candle_t
+
     type(c_ptr) :: surface, cr
     integer(c_int) :: status
     character(len=30), parameter :: filename = "output/chart.png"//char(0)
-
     integer, parameter :: n = 5
-    real(c_double), dimension(n) :: x, open, high, low, close
+    type(candle_t), dimension(n) :: candles
     integer :: i
     real(c_double) :: min_price, max_price
     integer, parameter :: img_width = 300, img_height = 200
 
-    ! ------------------------
-    ! Пример данных OHLC
-    ! ------------------------
-    x     = (/50.0d0, 100.0d0, 150.0d0, 200.0d0, 250.0d0/)
-    open  = (/10.0d0, 12.0d0, 11.0d0, 12.5d0, 13.0d0/)
-    close = (/12.0d0, 11.0d0, 12.5d0, 13.0d0, 10.0d0/)
-    high  = (/12.5d0, 12.0d0, 13.0d0, 13.5d0, 13.5d0/)
-    low   = (/9.5d0, 10.5d0, 11.0d0, 12.0d0, 12.0d0/)
+    ! Заполнение массива свечей
+    candles(1) = candle_t(50.0d0, 10.0d0, 12.5d0, 9.5d0, 12.0d0)
+    candles(2) = candle_t(100.0d0, 12.0d0, 12.0d0, 10.5d0, 11.0d0)
+    candles(3) = candle_t(150.0d0, 11.0d0, 13.0d0, 11.0d0, 12.5d0)
+    candles(4) = candle_t(200.0d0, 12.5d0, 13.5d0, 12.0d0, 13.0d0)
+    candles(5) = candle_t(250.0d0, 13.0d0, 13.5d0, 12.0d0, 10.0d0)
 
-    ! диапазон цен
-    min_price = minval(low)
-    max_price = maxval(high)
+    ! Определение min и max цен
+    min_price = minval(candles%low)
+    max_price = maxval(candles%high)
 
-    ! поверхность PNG
+    ! Создание поверхности и контекста Cairo
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, img_width, img_height)
     cr = cairo_create(surface)
 
-    ! фон
+    ! Фон
     call cairo_set_source_rgb(cr, 8/255d0, 22/255d0, 32/255d0)
     call cairo_rectangle(cr, 0.0d0, 0.0d0, real(img_width, c_double), real(img_height, c_double))
     call cairo_fill(cr)
 
-    ! ключевые уровни
+    ! Ключевые уровни
     call draw_levels(cr, min_price, max_price, 5, real(img_height, c_double))
 
-    ! свечи
+    ! Рисуем свечи
     do i = 1, n
-        call draw_candle(cr, x(i), open(i), high(i), low(i), close(i), &
-                         min_price, max_price, real(img_height, c_double))
+        call draw_candle(cr, candles(i)%x, candles(i)%open, candles(i)%high, &
+                         candles(i)%low, candles(i)%close, min_price, max_price, &
+                         real(img_height, c_double))
     end do
 
-    ! сохранить PNG
+    ! Сохранение PNG
     status = cairo_surface_write_to_png(surface, filename)
-
     call cairo_destroy(cr)
     call cairo_surface_destroy(surface)
 end program main
